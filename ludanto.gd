0397794b97034feb633ce76dbb68967fc9b8851d
Perglaz <perglaz@yahoo.fr>
2025-02-24 08:33:01 +0100

    initial commit

---
+++ ludanto.gd
@@ -0,0 +1,47 @@
+extends Area2D
+signal hit 
+@export var speed = 400 
+var screen_size 
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("movi_dekstren"):
+		velocity.x+=1
+	if Input.is_action_pressed("movi_maldekstren"):
+		velocity.x-=1
+	if Input.is_action_pressed("movi_supren"):
+		velocity.y-=1
+	if Input.is_action_pressed("movi_suben"):
+		velocity.y+=1
+	
+	if velocity.length()>0 : 
+		velocity = velocity.normalized()*speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x!=0:
+		$AnimatedSprite2D.animation = "marsxi"
+		$AnimatedSprite2D.flip_v=false
+		$AnimatedSprite2D.flip_h=velocity.x<0 
+	elif velocity.y !=0:
+		$AnimatedSprite2D.animation = "supre"
+		$AnimatedSprite2D.flip_v = velocity.y>0
+
+
+@warning_ignore("unused_parameter")
+func _on_body_entered(body: Node2D) -> void:
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled",true)
+	
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
